0dcd9c87ef4f0212305d236c07615c3d59150431
NjokuAnthony <a_njokuchibuzor@yahoo.com>
2023-05-22 23:56:18 +0100

    sixteen times writing a server path

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const http = require("http")
 
-http.createServer = ((request, respond) => {
-    respond.end("this is the sixth time i am working on creating a server")
-}).listen(6000)
-
-
-console.log("well don Tony")
+http.createServer((request, respond)=> {
+    respond.end("i have ")
+})
